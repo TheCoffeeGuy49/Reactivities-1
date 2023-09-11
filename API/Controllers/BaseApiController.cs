d91874eb2c35bfba8c7e74c2e250f0983a7c46b8
Dylan M <dyls.martin.49@gmail.com>
2023-09-11 06:04:26 +0200

    Attempting to upload all project files

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Shift + Alt + F: format code!
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
